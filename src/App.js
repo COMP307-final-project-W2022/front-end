ccc6507dd02fe32163fa6a66d0f3f97a5b339fe0
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-05 12:16:16 -0400

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 import bg from "./asset/background.png";
 import logo from "./asset/logo.png";
 
@@ -15,7 +16,13 @@ const Information = () => {
   return (
     <div className="info">
       <h1>MCGILL TA MANAGEMENT SYSTEM</h1>
-      <p>This system allows you to blah blah blah some copywritingCupidatat mollit sint fugiat commodo anim.Est ut anim anim labore deserunt adipisicing pariatur velit irure consequat voluptate id irure. Ad labore occaecat officia id dolor ut fugiat pariatur nostrud ullamco incididunt officia consectetur. Sint aliquip do tempor fugiat in ut.</p>
+      <p>
+        This system allows you to blah blah blah some copywritingCupidatat
+        mollit sint fugiat commodo anim.Est ut anim anim labore deserunt
+        adipisicing pariatur velit irure consequat voluptate id irure. Ad labore
+        occaecat officia id dolor ut fugiat pariatur nostrud ullamco incididunt
+        officia consectetur. Sint aliquip do tempor fugiat in ut.
+      </p>
     </div>
   );
 };
@@ -23,18 +30,30 @@ const Information = () => {
 const Login = () => {
   return (
     <div className="login-card">
-      <a href="#">REGISTER</a>
+      <nav>
+        <Link to="/register">Register here</Link>
+      </nav>
       <p>LOGIN</p>
       <form className="login-register">
         <ul>
           <li>
-            <label for="uname">Username</label>
-            <input type="text" name="uname" maxlength="100" autocomplete="off" />
+            <label htmlFor="uname">Username</label>
+            <input
+              type="text"
+              name="uname"
+              maxLength="100"
+              autoComplete="off"
+            />
             <span>Enter your username here</span>
           </li>
           <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" maxlength="100" autocomplete="off" />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              maxLength="100"
+              autoComplete="off"
+            />
             <span>Enter your password here</span>
           </li>
           <li>
@@ -49,33 +68,57 @@ const Login = () => {
 const Register = () => {
   return (
     <div className="register-card">
-      <a href="#">Back to Login</a>
+      <nav>
+        <Link to="/">Back to Login</Link>
+      </nav>
       <p>REGISTER</p>
       <form className="login-register register">
         <ul>
           <li>
-            <label for="name">Full Name</label>
-            <input type="text" name="name" maxlength="100" autocomplete="off" required/>
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              maxLength="100"
+              autoComplete="off"
+              required
+            />
             <span>Enter your full name here</span>
           </li>
           <li>
-            <label for="email">McGill Email</label>
-            <input type="email" name="email" maxlength="100" autocomplete="off" required/>
+            <label htmlFor="email">McGill Email</label>
+            <input
+              type="email"
+              name="email"
+              maxLength="100"
+              autoComplete="off"
+              required
+            />
             <span>Enter your McGill email here</span>
           </li>
           <li>
-            <label for="id">Student ID</label>
-            <input type="number" name="id" maxlength="9" autocomplete="off" />
+            <label htmlFor="id">Student ID</label>
+            <input type="number" name="id" maxLength="9" autoComplete="off" />
             <span>Enter your student ID here</span>
           </li>
           <li>
-            <label for="uname">Username</label>
-            <input type="text" name="uname" maxlength="100" autocomplete="off" />
+            <label htmlFor="uname">Username</label>
+            <input
+              type="text"
+              name="uname"
+              maxLength="100"
+              autoComplete="off"
+            />
             <span>Enter your username here</span>
           </li>
           <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" maxlength="100" autocomplete="off" />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              maxLength="100"
+              autoComplete="off"
+            />
             <span>Enter your password here</span>
           </li>
           <li>
@@ -87,14 +130,30 @@ const Register = () => {
   );
 };
 
+const CourseAssociation = () => {
+  return (
+    <div className="login-register">
+      <p>Select courses in which you are registered</p>
+      <label>Courses</label>
+      <textarea>Select courses...</textarea>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div></div>;
+};
+
 function App() {
   return (
     <div className="App" style={{ background: `url(${bg})` }}>
       <Header></Header>
       <div className="main">
-        <Information></Information>
-        <Register></Register>
-        {/* <Login></Login> */}
+        <Information />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </div>
     </div>
   );
