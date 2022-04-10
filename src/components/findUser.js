099859d905c0ab093e0ccbd4d174f9280727f5db
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-10 15:44:12 -0400

    find user

---
+++ src/components/findUser.js
@@ -0,0 +1,27 @@
+import { colourOptions } from "../asset/test-data.ts";
+import Select from "react-select";
+import glass from "../asset/feature-stickers/Magnifying Glass.png";
+
+const FindUser = () => {
+  return (
+    <div className="feature-card">
+      <img src={glass} alt="Magnifying Glass" />
+      <div className="select-course-container">
+        <p>Find a user</p>
+        <Select
+          className="select-course"
+          classNamePrefix="manage"
+          isLoading={false}
+          isClearable={true}
+          isRtl={true}
+          isSearchable={true}
+          name="choices"
+          options={colourOptions}
+        />
+        <button className="button-style">Go</button>
+      </div>
+    </div>
+  );
+};
+
+export default FindUser;
