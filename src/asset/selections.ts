b1f3e2622154dfe3ded4d0de3c8835ceee90899d
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-12 07:57:20 -0400

    add selection for edit user

--- src/asset/selections.ts
+++ src/asset/selections.ts
@@ -10,3 +10,17 @@ export const manageUserOptions: readonly ManageUserOption[] = [
     { value: 'edit', label: 'edit user', isFixed: true  },
     { value: 'delete', label: 'delete user', isFixed: true },
 ];
+
+export interface EditUserOption {
+    readonly value: string;
+    readonly label: string;
+    readonly isFixed?: boolean;
+    readonly isDisabled?: boolean;
+}
+
+export const editUserOptions: readonly EditUserOption[] = [
+    { value: 'email', label: 'email', isFixed: true },
+    { value: 'fname', label: 'first name', isFixed: true  },
+    { value: 'lname', label: 'last name', isFixed: true },
+    { value: 'mcgill-id', label: 'mcgill-id', isFixed: true },
+];
