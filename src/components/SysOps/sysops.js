fee456643da411e43a0c8771b35b4dce88005293
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-12 07:57:41 -0400

    add more redirection for sysops

--- src/components/SysOps/sysops.js
+++ src/components/SysOps/sysops.js
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import ManageUser from "./ManageUser/manageUser";
 import QuickImport from "./quickImport";
 import ManualImport from "./manualImport";
+import FindUser from "./ManageUser/findUser";
+import AddUser from "./ManageUser/addUser";
+import EditUser from "./ManageUser/editUser";
+import DeleteUser from "./ManageUser/deleteUser";
+import Success from "../success";
 
 const Sysops = () => {
   return (
@@ -10,6 +15,12 @@ const Sysops = () => {
         <Route path="/sysops/manage" element={<ManageUser />} />
         <Route path="/sysops/import" element={<QuickImport />} />
         <Route path="/sysops/manual" element={<ManualImport />} />
+        <Route path="/sysops/manage/find" element={<FindUser />} />
+        <Route path="/sysops/manage/add" element={<AddUser />} />
+        <Route path="/sysops/manage/edit" element={<EditUser />} />
+        <Route path="/sysops/manage/delete" element={<DeleteUser />} />
+        <Route path="/sysops/manage/deleted" element={<Success btn1="Delete Another User" btn2="Log Out"/>} />
+        <Route path="/sysops/manage/added" element={<Success btn1="Add Another User" btn2="Log Out"/>} />
       </Routes>
     </div>
   );
