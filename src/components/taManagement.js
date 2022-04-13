f3e943f1ec602b99c9a4308e86e6bef170bb0b07
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-04-13 13:02:44 +0600

    Fix routes

--- src/components/taManagement.js
+++ src/components/taManagement.js
@@ -8,128 +8,118 @@ import book from "../asset/feature-stickers/Brainstorming.png";
 
 const courseCode = "SMTH 123";
 const term = "Winter";
-const year = "2022"
+const year = "2022";
 
 const TAmanageFeature = (props) => {
-    return (
-      <div>
-        <div className="rate-card">
-          <h1>{props.title}</h1>
-          <div className="feature-description" style={{ textAlign: "center" }}>{props.description}</div>
-          <img width="150" height="150" src={props.image}></img>
-          <Link to={props.path}>
-            <button>Go</button>
-          </Link>
+  return (
+    <div>
+      <div className="rate-card">
+        <h1>{props.title}</h1>
+        <div className="feature-description" style={{ textAlign: "center" }}>
+          {props.description}
         </div>
+        <img width="150" alt="rate" height="150" src={props.image}></img>
+        <Link to={props.path}>
+          <button>Go</button>
+        </Link>
       </div>
-    );
-  };
-
+    </div>
+  );
+};
 
 const CourseTermSelection = () => {
-    return (
-        <div className="feature-card">
-        <div>
-        <p>Select Course and Term</p>     
+  return (
+    <div className="feature-card">
+      <div>
+        <p>Select Course and Term</p>
         <img src={book} alt="A book" />
-        </div>    
-          <div className="select-course-container">
-                <div className="select-term-container">
-                    <div className ="term-year">
-                        <p>Term</p>
-                        <Select
-                        className="select-term"
-                        classNamePrefix="term"
-                        isLoading={false}
-                        isClearable={true}
-                        isRtl={true}
-                        isSearchable={true}
-                        name="term"
-                        options={colourOptions}
-                        />
-                    </div>
-                    <div className = "term-year">
-                        <p>Year</p>
-                        <Select
-                        className="select-year"
-                        classNamePrefix="year"
-                        isLoading={false}
-                        isClearable={true}
-                        isRtl={true}
-                        isSearchable={true}
-                        name="year"
-                        options={colourOptions}
-                        />
-                    </div>
-                </div>
-                <p>Select a Course</p>
-                <Select
-                className="select-course"
-                classNamePrefix="course"
-                isLoading={false}
-                isClearable={true}
-                isRtl={true}
-                isSearchable={true}
-                name="course"
-                options={colourOptions}
-                />
-                <button className="button-style">Go</button>
-            </div>
+      </div>
+      <div className="select-course-container">
+        <div className="select-term-container">
+          <div className="term-year">
+            <p>Term</p>
+            <Select
+              className="select-term"
+              classNamePrefix="term"
+              isLoading={false}
+              isClearable={true}
+              isRtl={true}
+              isSearchable={true}
+              name="term"
+              options={colourOptions}
+            />
+          </div>
+          <div className="term-year">
+            <p>Year</p>
+            <Select
+              className="select-year"
+              classNamePrefix="year"
+              isLoading={false}
+              isClearable={true}
+              isRtl={true}
+              isSearchable={true}
+              name="year"
+              options={colourOptions}
+            />
+          </div>
         </div>
-    );
+        <p>Select a Course</p>
+        <Select
+          className="select-course"
+          classNamePrefix="course"
+          isLoading={false}
+          isClearable={true}
+          isRtl={true}
+          isSearchable={true}
+          name="course"
+          options={colourOptions}
+        />
+        <button className="button-style">Go</button>
+      </div>
+    </div>
+  );
 };
-
-const selectedCourseTerm = () => {
-    return (
-        <div className="greet">
-           Course: {courseCode} <br /> {term}{year}
-        </div>
-    );
-};
-
 
 const TaManagement = () => {
-    return (
-        <div> 
-        <Routes>   
-        <Route path="/taManagement/select" element={<CourseTermSelection />}></Route>  
-        <Route
-            path="/taManagement/options"
-            element={
-                <div className="TAmanageOptions"> 
-                    <div className="greet">
-                        {courseCode} <br /> {term} {year}
-                    </div>
-                    <div className="TAmanageFeatures">
-                        <TAmanageFeature
-                            title="Office Hours and Responsiblities "
-                            description="Rate any TAs from your registered courses anonymously. You will leave a
+  return (
+    <Routes>
+      <Route path="/select" element={<CourseTermSelection />} />
+      <Route
+        path="/options"
+        element={
+          <div className="TAmanageOptions">
+            <div className="greet">
+              {courseCode} <br /> {term} {year}
+            </div>
+            <div className="TAmanageFeatures">
+              <TAmanageFeature
+                title="Office Hours and Responsiblities "
+                description="Rate any TAs from your registered courses anonymously. You will leave a
                         rating on the scale of 0 to 5. Optionally, you can leave a short
                         review of 100 words and less."
-                            image={resp}
-                            path="/taManagement/editOh"
-                        />
-                        <br />
-                        <TAmanageFeature
-                            title="TA WishList"
-                            description="Exercitation velit ullamco anim laborum ullamco non. Duis in id aute commodo culpa irure irure incididunt enim aliquip officia."
-                            image={wish}
-                            path ="/taManagement/wishlist"
-                        />
-                        <br />
-                        <TAmanageFeature
-                            title="TA Performance"
-                            description="Exercitation velit ullamco anim laborum ullamco non. Duis in id aute commodo culpa irure irure incididunt enim aliquip officia."
-                            image={performance}
-                            path="/taManagement/taPerformance"
-                        />
-                    </div>
-                </div>
-            }
-            />
-            </Routes>
-      </div> 
-    );
-  };
-  
-  export default TaManagement;
+                image={resp}
+                path="/editOh"
+              />
+              <br />
+              <TAmanageFeature
+                title="TA WishList"
+                description="Exercitation velit ullamco anim laborum ullamco non. Duis in id aute commodo culpa irure irure incididunt enim aliquip officia."
+                image={wish}
+                path="/wishlist"
+              />
+              <br />
+              <TAmanageFeature
+                title="TA Performance"
+                description="Exercitation velit ullamco anim laborum ullamco non. Duis in id aute commodo culpa irure irure incididunt enim aliquip officia."
+                image={performance}
+                path="/taPerformance"
+              />
+            </div>
+          </div>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default TaManagement;
