8fbdcf462cc88b904d157bb218754914ee6bc33f
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-04-13 13:22:54 +0600

    Fix editOh routes

--- src/components/ohResp.js
+++ src/components/ohResp.js
@@ -2,35 +2,37 @@ import Success from "./success";
 import TableOh from "./tableOh.js";
 import { Routes, Route, Link } from "react-router-dom";
 
-const EditOh = () => { 
-    return (
-        <div className="feature-card">
-            <div>
-                <p> Edit OH and Responsibilties </p>
-                <TableOh />
-                <br />
-                <Link to="/taManagement/submitted">
-                    <button className="button-style" style={{ float:'right'}}>Submit</button>
-                </Link>
-                
-            </div>
-        </div>
-    );
+const EditOh = () => {
+  return (
+    <div className="feature-card">
+      <div>
+        <p> Edit OH and Responsibilties </p>
+        <TableOh />
+        <br />
+        <Link to="/submitted">
+          <button className="button-style" style={{ float: "right" }}>
+            Submit
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 const OHrespons = () => {
-    return (
-        <div>
-        <Routes>
-            <Route path="/taManagement/editOh" element={<EditOh />}></Route>
-            <Route
-            path="/taManagement/submitted"
-            element={<Success btn1="GO BACK TO OPTIONS" btn2="GO BACK TO DASHBOARD" />}
-          ></Route>
-        </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<EditOh />}></Route>
+        <Route
+          path="/submitted"
+          element={
+            <Success btn1="GO BACK TO OPTIONS" btn2="GO BACK TO DASHBOARD" />
+          }
+        ></Route>
+      </Routes>
+    </div>
+  );
 };
 
-
-export default OHrespons
+export default OHrespons;
