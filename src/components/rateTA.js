d24598ef47872e53e434947744cf3c95a701932e
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-04-16 19:30:43 +0600

    Completed TA Rating

--- src/components/rateTA.js
+++ src/components/rateTA.js
@@ -1,18 +1,35 @@
 import Select from "react-select";
-import { colourOptions } from "../asset/test-data.ts";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 import Rating from "@mui/material/Rating";
 
 import Success from "./success";
 
 import book from "../asset/feature-stickers/Brainstorming.png";
 import computer from "../asset/feature-stickers/KanbanBoard.png";
+import { query } from "../api";
+import { useState } from "react";
+import SplashScreen from "./splashScreen";
 
-const courseCode = "SMTH 123";
-const term = "Winter 2022";
-const taName = "John Doe";
+let courseCode = null;
+let term = null;
+let taName = null;
+let taUsername = null;
 
 const CourseSelection = () => {
+  const [courses, setCourses] = useState(null);
+
+  const getCourses = async () => {
+    if (courses != null) return;
+    const _courses = await query(
+      "select * from (Select distinct term, cid, username from courseusers where usertype='TA') a left join (select username, firstname, lastname from users) b on a.username=b.username"
+    );
+    setCourses(_courses);
+  };
+
+  getCourses();
+
+  if (courses == null) return <SplashScreen />;
+
   return (
     <div className="feature-card">
       <img src={book} alt="A book" />
@@ -21,12 +38,26 @@ const CourseSelection = () => {
         <Select
           className="select-course"
           classNamePrefix="course"
-          isLoading={false}
-          isClearable={true}
-          isRtl={true}
           isSearchable={true}
           name="course"
-          options={colourOptions}
+          onChange={(e) => {
+            courseCode = e.value.cid;
+            term = e.value.term;
+            taName = e.value.firstname + " " + e.value.lastname;
+            taUsername = e.value.username;
+          }}
+          options={courses.map((course) => {
+            return {
+              label: `${course.cid} - ${course.term} - ${course.firstname} ${course.lastname}`,
+              value: {
+                cid: course.cid,
+                term: course.term,
+                username: course.username,
+                firstname: course.firstname,
+                lastname: course.lastname,
+              },
+            };
+          })}
         />
         <Link to="../main">
           <button className="button-style">Rate</button>
@@ -37,6 +68,22 @@ const CourseSelection = () => {
 };
 
 const TARating = () => {
+  const [rating, setRating] = useState(null);
+  const [comment, setComment] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const submitReview = async () => {
+    setIsLoading(true);
+    if (rating == null) return;
+    await query(
+      `insert into tarating (rid, cid, term, tusername, score, tcomment) values ('${Date.now()}', '${courseCode}', '${term}', '${taUsername}', '${rating}', '${comment}')`
+    );
+    return navigate("../submitted", { replace: true });
+  };
+
+  if (courseCode == null) return <Navigate to="../select" />;
+
   return (
     <div className="feature-card">
       <img src={computer} alt="Computer" />
@@ -47,17 +94,30 @@ const TARating = () => {
           TA: {taName}
         </p>
         <h3>Rate the TA on a scale of 0 to 5</h3>
-        <Rating name="simple-controlled size-large" max={6} />
+        <Rating
+          onChange={(e) => setRating(e.target.value)}
+          name="simple-controlled size-large"
+          max={5}
+        />
         <br />
         <label for="review">Short Review (1000 characters limit)</label>
         <br />
-        <textarea id="review" name="review" rows="10">
-          Leave a short review here...
-        </textarea>
+        <textarea
+          id="review"
+          name="review"
+          placeholder="Leave a short review here..."
+          rows="10"
+          onChange={(e) => setComment(e.target.value)}
+        ></textarea>
         <br />
-        <Link to="/submitted">
-          <button className="button-style">Submit</button>
-        </Link>
+
+        <button
+          disabled={isLoading}
+          className="button-style"
+          onClick={submitReview}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
@@ -72,7 +132,12 @@ const RateTA = () => {
         <Route
           path="/submitted"
           element={
-            <Success btn1="GIVE ANOTHER RATING" btn2="GO BACK TO DASHBOARD" />
+            <Success
+              btn1="GIVE ANOTHER RATING"
+              btn2="GO BACK TO DASHBOARD"
+              location1="../select"
+              location2="/dashboard"
+            />
           }
         ></Route>
       </Routes>
