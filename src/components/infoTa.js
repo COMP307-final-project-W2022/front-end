4bfe159ce17c7b17cb8ffcff7700105b05036826
Firzana Sadik <firzana.sadik@mail.mcgill.ca>
2022-04-16 18:24:51 -0400

    Added TA History Feature

--- src/components/infoTa.js
+++ src/components/infoTa.js
@@ -8,6 +8,106 @@ import wish from "../asset/feature-stickers/wish.png";
 
 const name = "Jane Doe"
 
+const StudentRemarks = () => {
+    return(
+        <div className="feature-card"> 
+        <div style={{marginRight:"100px"}}>
+            <p> TA: {name} </p>
+            <img style={{height:"400px", width:"400px"}} src={checklist}></img> 
+        </div>
+        <div>
+            <p>Student Remarks</p>
+            <table className="all-table">
+                <tr className= "all-tr"> <td>COMP250</td> </tr> 
+                <tr className= "all-tr"> <td> COMP202 </td> </tr>
+                <tr className= "all-tr"> <td> COMP360 </td> </tr>
+                <tr className= "all-tr"> <td> COMP303</td> </tr>
+            </table>
+            <br />
+            <Link to="/taAdmin/info">
+                <button style={{float:"right"}} className="button-style">Back</button>
+            </Link>
+        </div>
+    </div>
+    );
+};
+
+
+const History = () => {
+    return (
+        <div className = "feature-card">
+            <div>
+            <p style={{marginBottom:'20px', position:'center'}}> TA: {name} </p>
+            <table className="ta-history-table">
+            <thead style={{position:'sticky'}}>
+                <tr className="ta-history-row">
+                <th scope="col">Previous Courses</th>
+                <th scope="col">Term</th>
+                <th scope="col">Avg Student Rating </th>
+                <th scope="col">Ta Performance</th>
+                </tr>
+            </thead>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td > 10 <button className="view-student-button"> view </button></td>
+                    <td> Very Well </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button></td>
+                    <td> Very Well, Manage to do here Work on time and efficiently and excitingkejf akjfksjkjf
+                        jlfjkdkjfkfj kjsdkfjksfj ouorfioioiofi rrrrr ladlklaksd saopd </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button></td>
+                    <td> Very Well </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button></td>
+                    <td> Very Well </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button></td>
+                    <td> Very Well </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button></td>
+                    <td> Very Well </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button> </td>
+                    <td> Very Well </td>
+                </tr>
+                <tr className="ta-history-row"> 
+                    <td> COMP250 </td>
+                    <td> FALL2022 </td>
+                    <td> 10 <button className="view-student-button"> view </button> </td>
+                    <td> Very Well </td>
+                </tr>
+            </table>
+            <br/>
+            <Link to="/taAdmin/info">
+                <button style={{float:'right'}} className="button-style">Back</button>
+            </Link>
+            
+            </div>
+        </div>
+    );
+};
+
+
 const WishList = () => {
     return (
         <div className="feature-card"> 
@@ -17,16 +117,18 @@ const WishList = () => {
             </div>
             <div>
                 <p>Courses of Professors WishList</p>
-                <table>
-                    <tr> sup what is your name </tr> 
-                    <tr> sup my name is  </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> how arr you </tr>
+                <table className="all-table">
+                    <thead>
+                        <tr> <th> Course </th> <th> Term </th></tr>
+                    </thead>
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr> 
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr>
+                    <tr className= "all-tr"> <td> COMP202 </td> <td> FALL2022 </td></tr>
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr>
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr>
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr>
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr>
+                    <tr className= "all-tr"> <td> COMP202 </td>  <td> FALL2022 </td></tr>
                 </table>
                 <br />
                 <Link to="/taAdmin/info">
@@ -47,16 +149,11 @@ const CurrentTa = () => {
             </div>
             <div>
                 <p> Current Courses</p>
-                <table>
-                    <tr> sup what is your name </tr> 
-                    <tr> sup my name is  </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> sup </tr>
-                    <tr> how arr you </tr>
+                <table className="all-table">
+                    <tr className= "all-tr"> <td>COMP250</td> </tr> 
+                    <tr className= "all-tr"> <td> COMP202 </td> </tr>
+                    <tr className= "all-tr"> <td> COMP360 </td> </tr>
+                    <tr className= "all-tr"> <td> COMP303</td> </tr>
                 </table>
                 <br />
                 <Link to="/taAdmin/info">
@@ -118,6 +215,8 @@ const InfoTa = () => {
             <Route path="/taAdmin/info" element={<SelectTa />}/>
             <Route path="/taAdmin/info/current" element={<CurrentTa />}/>
             <Route path="/taAdmin/info/wishlist" element={<WishList />}/>
+            <Route path="/taAdmin/info/history" element={<History />}/>
+            <Route path="/taAdmin/info/remarks" element={<StudentRemarks />}/>
         </Routes>
     );
 };
