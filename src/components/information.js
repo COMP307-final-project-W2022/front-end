64058441f640ebc34f2dbd7d7a486110f2297aaa
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-17 15:13:10 -0400

    add copywriting for landing page

--- src/components/information.js
+++ src/components/information.js
@@ -3,11 +3,9 @@ const Information = () => {
     <div className="info">
       <h1>MCGILL TA MANAGEMENT SYSTEM</h1>
       <p>
-        This system allows you to blah blah blah some copywritingCupidatat
-        mollit sint fugiat commodo anim.Est ut anim anim labore deserunt
-        adipisicing pariatur velit irure consequat voluptate id irure. Ad labore
-        occaecat officia id dolor ut fugiat pariatur nostrud ullamco incididunt
-        officia consectetur. Sint aliquip do tempor fugiat in ut.
+        This system can handle an array of actions from different types of users, students, TAs, professors, TA administrators, 
+        as well as system operators. Some examples of operations supported by this system include: Student can submit ratings and reviews for their TAs.
+        Professor can rate TA performances. TAs can add a course to their wishlist. And many more.
       </p>
     </div>
   );
